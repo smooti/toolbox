f30ece0382ad494c429ec690b6196653728dbf95
smooti <kobeowens43099@gmail.com>
2025-04-29 16:29:18 -0400

    Adding vars

--- main.go
+++ main.go
@@ -50,6 +50,11 @@ func GetDiskInfo(drive ...string) error {
 		targetDrive = "/" // Default for other systems (Linux, macOS, etc.)
 	}
 
+	// We just want to perform operations on a single drive
+	if len(drive) > 0 {
+		targetDrive = drive[0]
+	}
+
 	usage, err := disk.Usage(targetDrive)
 	if err != nil {
 		log.Fatal(err)
@@ -59,12 +64,14 @@ func GetDiskInfo(drive ...string) error {
 	freeDiskSpace := usage.Free / 1024 / 1024 / 1024
 	usedDiskSpace := usage.Used / 1024 / 1024 / 1024
 	usedDiskSpacePercentage := usage.UsedPercent
+	filesystemType := usage.Fstype
+	path := usage.Path
 
 	fmt.Println("Disk Information:")
 	fmt.Printf("  Total: %v GB\n", totalDiskUsage)
 	fmt.Printf("  Free: %v GB\n", freeDiskSpace)
 	fmt.Printf("  Used: %v GB (%.2f%%)\n", usedDiskSpace, usedDiskSpacePercentage)
-	fmt.Printf("  Filesystem: %s\n", usage.Fstype)
-	fmt.Printf("  Mount Point: %s\n", usage.Path)
+	fmt.Printf("  Filesystem: %s\n", filesystemType)
+	fmt.Printf("  Mount Point: %s\n", path)
 	return nil
 }
